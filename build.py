b55bb34dc1a04672df5c3059181a9c0233015256
Schnobb <mrbigpapaballs@gmail.com>
2025-11-29 22:16:45 -0500

    Fixed exclude list folder handling

--- build.py
+++ build.py
@@ -25,22 +25,27 @@ def createFileFromTemplate(file_name, template_file_name, placeholder, value):
 
 def main():
     excluded_files = []
+    excluded_folders = []
 
     if os.path.exists(FILE_EXCLUDE):
         f = open(FILE_EXCLUDE)
         excluded_files.extend(list(map(str.strip, f.read().split('\n'))))
         f.close()
 
-        excluded_files = [x for x in excluded_files if x != "" and not x.startswith("#")]
+        excluded_files = [x for x in excluded_files if x != "" and not x.startswith('#')]
         excluded_files = [normalizePath(x) for x in excluded_files]
+
+        excluded_folders = [x for x in excluded_files if x.endswith('/')]
 
     file_list = []
     root_dir = os.path.dirname(os.path.abspath(__file__))
     
     for root, dirs, files in os.walk(root_dir):
-        # skips folders that start with a '.' (like .git, etc.)
+        # skips folders that start with a '.' (like .git, etc.) or are in the exclude list
         for d in list(dirs):
-            if d.startswith('.'):
+            script_dir = normalizePath(os.path.dirname(os.path.abspath(__file__))) + "/"
+            base_path = normalizePath(os.path.join(root, d)).replace(script_dir, "") + "/"
+            if d.startswith('.') or base_path in excluded_folders:
                 dirs.remove(d)
 
         for f in files:
